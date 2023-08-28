ee1960bdfc68f70c48eec8ba0c94099ec86555d6
Mustafa Kan <79532179+mustafakan01@users.noreply.github.com>
2023-08-28 19:50:14 +0300

    Router

--- burgershop/src/App.jsx
+++ burgershop/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import { Home } from './Components/Home'
+import { Menü } from './Components/Menü'
+import { About } from './Components/About'
+import { Contact } from './Components/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <Routes>
+        <Route path='/' exact element={<Home></Home>} />
+        <Route path='/menu' exact element={<Menü></Menü>} />
+        <Route path='/about' exact element={<About></About>} />
+        <Route path='/contact' exact element={<Contact></Contact>} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ burgershop/src/Components/Menü.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Menü = () => {
+  return (
+    <div>Menü</div>
+  )
+}
